1d3cbe364d6ecc1d0b808c9bb41de5fe2bbff3bc
jonay <casa@gmail.com>
2017-03-27 12:31:39 +0100

    First commit

---
+++ javascriptAllogue_1.js
@@ -0,0 +1,14 @@
+'use strict';
+
+
+let should = require('chai').should();
+
+describe('A Rich Aroma: Basic Numbers', () => {
+
+
+
+it("test green",() => {
+    true.should.equal(true);
+});
+
+});
