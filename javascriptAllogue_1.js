357a9b2a941822b9396c5f42646c42a92e60a49e
jonay <casa@gmail.com>
2017-03-27 13:30:55 +0100

    functions can have grandparents too

--- javascriptAllogue_1.js
+++ javascriptAllogue_1.js
@@ -41,4 +41,11 @@ describe('The first sip: Basic Functions', function () {
         ((x) => (y) => x)(value_x)('y').should.equal(0);
     });
 
+    it('functions can have grandparents too', function () {
+        let [value_x, value_y, value_z] = [1, 2, 3];
+        let functionGrandfather = ((x) =>(y) =>(z) => x + y + z)(value_x)(value_y)(value_z);
+        let funcionWithoutFathers = ((x, y, z) => x + y + z)(value_x, value_y, value_z);
+        functionGrandfather.should.equal(funcionWithoutFathers);
+    });
+
 });
