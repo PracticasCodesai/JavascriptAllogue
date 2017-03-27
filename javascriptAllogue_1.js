c53deef8ae0635c3d32a5f30a7f0023810ee0649
jonay <casa@gmail.com>
2017-03-27 12:37:36 +0100

    bug float

--- javascriptAllogue_1.js
+++ javascriptAllogue_1.js
@@ -6,9 +6,15 @@ let should = require('chai').should();
 describe('A Rich Aroma: Basic Numbers', () => {
 
 
+    it("test green",() => {
+        true.should.equal(true);
+    });
 
-it("test green",() => {
-    true.should.equal(true);
-});
+
+    it('Bug float', function () {
+        (0.1+0.1+0.1).should.equal(0.30000000000000004);
+    });
+
+
 
 });
