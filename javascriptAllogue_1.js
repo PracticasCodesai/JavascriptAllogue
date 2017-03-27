bbc812a45d0ee7fb7b13684203c5c38e7da7304e
jonay <casa@gmail.com>
2017-03-27 12:46:05 +0100

    refactor fuctions test

--- javascriptAllogue_1.js
+++ javascriptAllogue_1.js
@@ -5,15 +5,17 @@ let should = require('chai').should();
 
 describe('A Rich Aroma: Basic Numbers', () => {
 
-
     it("test green",() => {
         true.should.equal(true);
     });
 
-
     it('Bug float', function () {
         (0.1+0.1+0.1).should.equal(0.30000000000000004);
     });
+
+});
+
+describe('The first sip: Basic Functions', function () {
 
     it('minimum function Javascript', function () {
         (() => 0).should.be.a('function');
