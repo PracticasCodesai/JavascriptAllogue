7be750f2d57185cf8d2136065fd24cdc9a703167
naker90 <AntonioJesusSG96@gmail.com>
2017-03-31 11:41:14 +0100

    we can write a different iterator for a different data structure, iterator

--- javascriptAllogue_1.js
+++ javascriptAllogue_1.js
@@ -994,4 +994,56 @@ describe('Composing and Decomposing Data', function () {
         iteratorSum(arrayIterator([1, 4, 9, 16, 25])).should.equal(55);
 
     });
+
+    it('we can write a different iterator for a different data structure, iterator', function () {
+        const EMPTY = null;
+
+        const isEmpty = (node) => node === EMPTY;
+
+        const pair = (first, rest = EMPTY) => ({first, rest});
+
+        const list = (...elements) => {
+            const [first, ...rest] = elements;
+
+            return elements.length === 0
+                ? EMPTY
+                : pair(first, list(...rest))
+        }
+
+        const print = (aPair) =>
+            isEmpty(aPair)
+                ? ""
+                : `${aPair.first} ${print(aPair.rest)}`
+
+        const listIterator = (aPair) =>
+            () => {
+                const done = isEmpty(aPair);
+                if (done) {
+                    return {done};
+                }
+                else {
+                    const {first, rest} = aPair;
+
+                    aPair = aPair.rest;
+                    return { done, value: first }
+                }
+            }
+
+        const iteratorSum = (iterator) => {
+            let eachIteration,
+                sum = 0;;
+
+            while ((eachIteration = iterator(), !eachIteration.done)) {
+                sum += eachIteration.value;
+            }
+            return sum
+        }
+
+        const aListIterator = listIterator(list(1, 4, 9, 16, 25));
+
+        iteratorSum(aListIterator).should.equal(55);
+    });
+
+
+
 });
