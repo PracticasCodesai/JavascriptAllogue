c41907b10a9e07e2453b7cbc8728ba295746d7b8
jonay <casa@gmail.com>
2017-04-05 10:04:53 +0100

    Example of an object with iterator

--- javascriptAllogue_1.js
+++ javascriptAllogue_1.js
@@ -1608,4 +1608,70 @@ describe('Recipes with Objects, Mutations, and State', function () {
         assert.deepEqual(deepMapWith(getWith('price'))(report), expected);
     });
 
+
+    it('Example of an object with iterator', function () {
+        const Stack1 = () =>
+            ({
+                array:[],
+                index: -1,
+                push (value) {
+                    return this.array[this.index += 1] = value;
+                },
+                pop () {
+                    const value = this.array[this.index];
+
+                    this.array[this.index] = undefined;
+                    if (this.index >= 0) {
+                        this.index -= 1
+                    }
+                    return value
+                },
+                isEmpty () {
+                    return this.index < 0
+                },
+                iterator () {
+                        let iterationIndex = this.index;
+
+                    var self = this;
+                    return {
+                        next () {
+                            if (iterationIndex > self.index) {
+                                iterationIndex = self.index;
+                            }
+                            if (iterationIndex < 0) {
+                                return {done: true};
+                            }
+                            else {
+                                return {
+                                    done: false,
+                                    value: self.array[iterationIndex--]
+                                }
+                            }
+                        }
+                    }
+
+                }
+            });
+
+        const stack = Stack1();
+
+
+        stack.push(2000);
+        stack.push(10);
+        stack.push(5);
+
+        const collectionSum = (collection) => {
+            const iterator = collection.iterator();
+
+            let eachIteration,
+                sum = 0;
+
+            while ((eachIteration = iterator.next(), !eachIteration.done)) {
+                sum += eachIteration.value;
+            }
+            return sum
+        };
+
+        collectionSum(stack).should.equal(2015);
+    });
 });
