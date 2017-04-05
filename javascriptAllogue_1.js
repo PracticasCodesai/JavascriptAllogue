1883c0e861b0982a69b59996c9067e629c713383
jonay <casa@gmail.com>
2017-04-05 10:15:13 +0100

    Javascript Iterator, [Symbol.iterator]

--- javascriptAllogue_1.js
+++ javascriptAllogue_1.js
@@ -1674,4 +1674,77 @@ describe('Recipes with Objects, Mutations, and State', function () {
 
         collectionSum(stack).should.equal(2015);
     });
+
+    it('Javascript Iterator, [Symbol.iterator]', function () {
+        const Stack3 = () =>
+            ({
+                array: [],
+                index: -1,
+                push (value) {
+                    return this.array[this.index += 1] = value;
+                },
+                pop () {
+                    const value = this.array[this.index];
+
+                    this.array[this.index] = undefined;
+                    if (this.index >= 0) {
+                        this.index -= 1
+                    }
+                    return value
+                },
+                isEmpty () {
+                    return this.index < 0
+                },
+                [Symbol.iterator] () {
+                    let iterationIndex = this.index;
+
+                    var self = this;
+                    return {
+                        next () {
+                            if (iterationIndex > self.index) {
+                                iterationIndex = self.index;
+                            }
+                            if (iterationIndex < 0) {
+                                return {done: true};
+                            }
+                            else {
+                                return {done: false, value: self.array[iterationIndex--]}
+                            }
+                        }
+                    }
+                }
+            });
+
+        const stack = Stack3();
+
+        stack.push(2000);
+        stack.push(10);
+        stack.push(5);
+
+        const collectionSum = (collection) => {
+            const iterator = collection[Symbol.iterator]();
+
+            let eachIteration,
+                sum = 0;
+
+            while ((eachIteration = iterator.next(), !eachIteration.done)) {
+                sum += eachIteration.value;
+            }
+            return sum
+        };
+
+        collectionSum(stack).should.equal(2015);
+
+        const iterableSum = (iterable) => {
+            let sum = 0;
+
+            for (const num of iterable) {
+                sum += num;
+            }
+            return sum
+        }
+
+        iterableSum(stack).should.equal(2015);
+    });
+    
 });
