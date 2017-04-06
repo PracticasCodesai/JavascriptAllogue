d781fd843abcbb35bc6833962021bbf4bf609425
jonay <casa@gmail.com>
2017-04-06 10:16:42 +0100

    Create private with Symbol

--- javascriptAllogue_1.js
+++ javascriptAllogue_1.js
@@ -1927,4 +1927,47 @@ describe('A Coffeehouse: Symbols', function () {
         expect(Symbol(allongue) !== Symbol(allongue)).to.be.true;
     });
 
+    it('Create private with Symbol', function () {
+        const array = Symbol(),
+            head  = Symbol(),
+            tail  = Symbol();
+
+        const Queue = () =>
+            ({
+                [array]: [],
+                [head]: 0,
+                [tail]: -1,
+                pushTail (value) {
+                    return this[array][this[tail] += 1] = value
+                },
+                pullHead () {
+                    if (this[tail] >= this[head]) {
+                        let value = this[array][this[head]];
+
+                        this[array][this[head]] = undefined;
+                        this[head] += 1;
+                        return value
+                    }
+                },
+                isEmpty () {
+                    return this[tail] < this[head]
+                }
+            });
+
+        let q = Queue();
+        q.pushTail('hello');
+        q.pushTail('symbols');
+
+        q.pullHead().should.equals("hello");
+
+        assert.deepEqual(q,{
+            "isEmpty": q.isEmpty,
+            "pullHead": q.pullHead,
+            "pushTail": q.pushTail
+        });
+
+        expect(q.tail).to.be.undefined;
+
+    });
+
 });
