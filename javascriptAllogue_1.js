ffbe6796d608e0ddff4afe81748c39a7df9f2d65
jonay <casa@gmail.com>
2017-04-06 11:11:09 +0100

    This function forwards methods to another object

--- javascriptAllogue_1.js
+++ javascriptAllogue_1.js
@@ -2046,5 +2046,46 @@ describe('Life on the Plantation: Metaobjects', function () {
         peck.career().should.equal('Director');
     });
 
+    it('This function forwards methods to another object', function () {
+        function forward (receiver, metaobject, ...methods) {
+            methods.forEach(function (methodName) {
+                receiver[methodName] = (...args) => metaobject[methodName](...args)
+            });
+
+            return receiver;
+        };
+
+        const portfolio = (function () {
+            const investments = Symbol();
+
+            return {
+                [investments]: [],
+                addInvestment (investment) {
+                    this[investments].push(investment);
+                },
+                netWorth () {
+                    return this[investments].reduce(
+                        function (acc, investment) {
+                            return acc + investment.value;
+                        },
+                        0
+                    );
+                }
+            };
+        })();
+
+        const investor = forward({}, portfolio, "addInvestment", "netWorth");
+
+        investor.addInvestment({ type: "art", value: 1000000 });
+        investor.addInvestment({ type: "art", value: 2000000 });
+        investor.netWorth().should.equal(3000000);
+
+        portfolio.netWorth = function () {
+            return "I'm actually bankrupt!";
+        }
+
+        investor.netWorth().should.equal("I'm actually bankrupt!");
+
+    });
 
 });
